6115d0c615acbb1c52cc3c61806921601b6d27d6
ZheRao <zheraoeminem@gmail.com>
2026-05-10 12:00:18 -0600

    placeholder

---
+++ src/data_platform/sources/qbo/ingestion/engine_pl.py
@@ -0,0 +1,13 @@
+"""
+src.data_platform.sources.qbo.ingestion.engine_pl
+
+Purpose:
+    - extraction raw PL report from QBO API endpoints
+
+Exposed API:
+    - `transform_pl_spark()` 
+    - `ingest_pl_pandas()`
+    
+Notes:
+    
+"""
